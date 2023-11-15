a9370df5eba1300d7f62de771b81f9fa29fd2fa9
vmarialuz <vmarialuzm@gmail.com>
2023-11-14 21:06:32 -0500

    [ADD] añadí ejercicio 47

--- retos-programacion-2022/Reto #47 - VOCAL MAS COMUN/ejercicio47.py
+++ retos-programacion-2022/Reto #47 - VOCAL MAS COMUN/ejercicio47.py
@@ -1,3 +1,21 @@
+def encontrar_vocal(texto):
+    vocales = ["a", "e", "i", "o", "u"]
+    dict_vocales = {}
+    if type(texto) == str:
+        for letra in texto:
+            if letra in vocales:
+                if letra in dict_vocales:
+                    dict_vocales[letra] += 1
+                else:
+                    dict_vocales[letra] = 1
 
+        if len(dict_vocales) == 0:
+            return ""
+        return f"La vocal que más se repite es la: {max(dict_vocales, key = dict_vocales.get)} y aparece {dict_vocales[max(dict_vocales, key = dict_vocales.get)]} veces"
+                
+
+
+print(encontrar_vocal("mazamorra"))
+print(encontrar_vocal("mhg"))
 
 
